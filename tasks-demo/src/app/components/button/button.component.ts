40bff2eb3a89935ef73819e0bde674f5cf69c911
Charan Karthik <secharank@gmail.com>
2023-02-23 17:11:20 -0800

    event listener and output flow added

--- tasks-demo/src/app/components/button/button.component.ts
+++ tasks-demo/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -9,4 +9,12 @@ export class ButtonComponent {
   @Input() text!: string;
   @Input() color!: string;
 
+  @Output() btnClick = new EventEmitter();
+  // not too sure what the EventEmitter is doing - will need to come back to this
+  // my idea is that it records the click as an event to send as an output for another component to take in as an input
+
+  onClick() {
+    // console.log('Add')
+    this.btnClick.emit();
+  }
 }
